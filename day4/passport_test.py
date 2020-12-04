57fc740d3f42e9594127684f6c0bad6f934031bc
thameena <thameena8@gmail.com>
2020-12-05 00:19:20 +0530

    day4

--- day4/passport_test.py
+++ day4/passport_test.py
@@ -5,7 +5,7 @@ from .passport import read_input
 
 class Test(TestCase):
     def test_valid_passport1(self):
-        input_text = read_input("day4/test_input.txt")
+        input_text = read_input("day4/test_input1.txt")
         self.assertEqual(count_valid_passports(input_text), 2)
 
     def test_valid_passport2(self):
